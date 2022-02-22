ac036e21c7e7996f5029bd8eab05e5a74ca2084c
motoki418 <snkrs418@gmail.com>
2022-02-22 21:45:43 +0900

    FirstCommit

---
+++ ScrollableMenu/ScrollableMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableMenuApp.swift
+//  ScrollableMenu
+//
+//  Created by nakamura motoki on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
